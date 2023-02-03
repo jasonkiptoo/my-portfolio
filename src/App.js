a5d9de645a3cb41543ac332ff1583c8f1b4c77f5
jasonkiptoo <45195231+jasonkiptoo@users.noreply.github.com>
2023-02-03 09:28:08 +0300

    added about me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './PortfolioContainer/Home/Home.js';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./PortfolioContainer/Home/Home.js";
+import AboutMe from "./PortfolioContainer/AboutMe/AboutMe";
 
 function App() {
   return (
     <div className="App">
- <Home/>
+      <Home />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+function AboutMe() {
+  return <div>
+    <h1>About Me</h1>
+  </div>;
+}
+
+export default AboutMe;
