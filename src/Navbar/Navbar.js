ac2f9e4a8f68503c7544b1a5ad0462371a61103b
jasonkiptoo <45195231+jasonkiptoo@users.noreply.github.com>
2023-02-04 20:43:45 +0300

    added thenavbar icons

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { AiOutlineHome } from "react-icons/ai";
-
+import {
+  AiOutlineHome,
+  AiOutlineContacts,
+  AiOutlineCustomerService,
+} from "react-icons/ai";
+import { HiTag } from "react-icons/hi";
 function Navbar() {
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
+      <a href="#">
+        <AiOutlineHome />
+      </a>{" "}
+      <a href="#about">{/* <FcAbout /> */}</a>{" "}
+      <a href="#book">
+        <HiTag />
+      </a>{" "}
+      <a href="#services">
+        <AiOutlineCustomerService />
+      </a>{" "}
+      <a href="#contact">
+        <AiOutlineContacts />
+      </a>
     </nav>
   );
 }
